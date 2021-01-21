0ad006ec7afea554752f7273432170c3bde36bb0
June Wunder <june@junewunder.com>
2021-01-21 13:50:11 -0500

    delete matterjs main

--- main.js
+++ main.js
@@ -1,101 +1,50 @@
-import {
-    Body,
-    Engine,
-    Render,
-    Runner,
-    Composite,
-    Composites,
-    Common,
-    Events,
-    MouseConstraint,
-    Mouse,
-    World,
-    Bodies,
-    Query,
-} from "matter-js";
-
+import * as PIXI from 'pixi.js'
+import { Engine, Runner, Body, Bodies, World } from 'matter-js'
 import { makeCharacter } from './src/character'
-import { makePlatform, makePlatformGenerator } from './src/platform'
+import { makePlatformGenerator } from './src/platform'
 
-const WIDTH = window.innerWidth - 50
-const HEIGHT = window.innerHeight - 50
+const WIDTH = window.innerWidth * 0.95
+const HEIGHT = window.innerHeight * 0.95
 
-var Main = Main || {};
+const pixi = new PIXI.Application({
+    antialias: true,
+    height: HEIGHT,
+    width: WIDTH,
+});
+const engine = Engine.create();
+const world = engine.world;
+const runner = Runner.create();
 
-Main.init = function () {
-    // create engine
-    const engine = Engine.create();
-    const world = engine.world;
-    world.gravity.y = 2
+Runner.run(runner, engine);
+world.gravity.y = 2
 
-    // create renderer
-    const render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            width: WIDTH,
-            height: HEIGHT,
-            showAngleIndicator: true,
-            showCollisions: true,
-        }
-    });
+const floor = Bodies.rectangle(WIDTH / 2, HEIGHT, WIDTH, 50, { isStatic: true })
+const leftWall = Bodies.rectangle(0, HEIGHT / 2, 50, HEIGHT, { isStatic: true })
+const rightWall = Bodies.rectangle(WIDTH, HEIGHT / 2, 50, HEIGHT, { isStatic: true })
 
-    Render.run(render);
+makeCharacter({ engine, pixi, x: WIDTH / 2, y: HEIGHT / 2 })
+makePlatformGenerator({ pixi, engine, floor, WIDTH })
 
-    // create runner
-    var runner = Runner.create();
-    Runner.run(runner, engine);
+World.add(world, [
+    floor,
+    leftWall,
+    rightWall,
+]);
 
-    const character = makeCharacter({ engine, x: WIDTH / 2, y: HEIGHT / 2 })
-    
-    Events.on(engine, 'collisionActive', (event) => {
-    });
-
-    const floor = Bodies.rectangle(WIDTH / 2, HEIGHT, WIDTH, 50, { isStatic: true })
-    const platformGenerator = makePlatformGenerator({ floor, engine, WIDTH })
-
-    World.add(world, [
-        // walls
-        // Bodies.rectangle(WIDTH / 2, 0, WIDTH, 50, { isStatic: true }),
-        floor,
-        Bodies.rectangle(WIDTH, HEIGHT / 2, 50, HEIGHT, { isStatic: true }),
-        Bodies.rectangle(0, HEIGHT / 2, 50, HEIGHT, { isStatic: true }),
-    ]);
-
-    // add mouse control
-    // var mouse = Mouse.create(render.canvas),
-    //     mouseConstraint = MouseConstraint.create(engine, {
-    //         mouse: mouse,
-    //         constraint: {
-    //             stiffness: 0.2,
-    //             render: {
-    //                 visible: false
-    //             }
-    //         }
-    //     });
-
-    // World.add(world, mouseConstraint);
-
-    // keep the mouse in sync with rendering
-    // render.mouse = mouse;
-
-    // fit the render viewport to the scene
-    // Render.lookAt(render, {
-    //     min: { x: 0, y: 0 },
-    //     max: { x: WIDTH, y: HEIGHT }
-    // });
-
-    // context for MatterTools.Demo
-    return {
-        engine: engine,
-        runner: runner,
-        // render: render,
-        canvas: render.canvas,
-        stop: function () {
-            // Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-        }
+const graphics = new PIXI.Graphics();
+pixi.stage.addChild(graphics);
+pixi.ticker.add(() => {
+    graphics.clear()
+    graphics.beginFill(0xDE3249);
+    for (const body of [floor, leftWall, rightWall]) {
+        graphics.drawRect(
+            body.bounds.min.x,
+            body.bounds.min.y,
+            body.bounds.max.x - body.bounds.min.x,
+            body.bounds.max.y - body.bounds.min.y
+        );
     }
-};
+    graphics.endFill();
+})
 
-Main.init()
+document.body.appendChild(pixi.view);
